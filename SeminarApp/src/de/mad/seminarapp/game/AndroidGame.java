fd0cdc88951c1b2d3beb7896585265d90fe29cdd
Stefan Reppner <stefanreppner@gmail.com>
2013-06-25 20:09:43 +0200

    libs for game framework plus init game classes

---
+++ SeminarApp/src/de/mad/seminarapp/game/AndroidGame.java
@@ -0,0 +1,10 @@
+package de.mad.seminarapp.game;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+
+public class AndroidGame extends AndroidApplication {
+	 public void onCreate (android.os.Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         initialize(new Game(), false);
+ }
+}
